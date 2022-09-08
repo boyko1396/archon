8e8a3f182423fb5dabd03c18fd86013f4e8309ce
Vasya <boyko1396@gmail.com>
2022-09-08 21:19:43 +0300

    add email and redirect

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-  // toggle display
-  $('.js-target-services').on('click', function (e) {
-    $('.about').addClass('is-hide');
-    $('.main, .services').addClass('is-show-first');
-    setTimeout(function () {
-        $('.services').addClass('is-show-second');
-    }, 2500);
-    e.preventDefault();
-  });
+    // toggle display
+    $('.js-target-services').on('click', function (e) {
+        $('.about').addClass('is-hide');
+        $('.main, .services').addClass('is-show-first');
+        setTimeout(function () {
+            $('.services').addClass('is-show-second');
+        }, 2500);
+        e.preventDefault();
+    });
 
-  $('.js-target-wallet').on('click', function (e) {
-    $('.wallet').addClass('is-show');
-    $('.services').addClass('is-hide');
-    $('.main').addClass('is-show-three');
-    e.preventDefault();
-  });
+    $('.js-target-wallet').on('click', function (e) {
+        $('.wallet').addClass('is-show');
+        $('.services').addClass('is-hide');
+        $('.main').addClass('is-show-three');
+        e.preventDefault();
+    });
 });
